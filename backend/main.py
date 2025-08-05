414b1b502675d2b6020081d144a91093d4eb5124
nikitakuvsh <world_fre@mail.ru>
2025-08-05 18:37:09 +0300

    try prod

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 from .routers import items
 from .database import Base, engine
 import os
@@ -20,6 +21,13 @@ app.add_middleware(
 
 # Подключение роутера
 app.include_router(items.router)
+
+frontend_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../frontend"))
+app.mount("/static", StaticFiles(directory=frontend_path), name="static")  # ✅ правильно
+
+@app.get("/")
+def serve_index():
+    return FileResponse(os.path.join(frontend_path, "index.html"))
 
 # Статические файлы
 # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
