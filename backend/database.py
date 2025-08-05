30e6699dbe81e217a611fb8fd9f3044c9150bc61
nikitakuvsh <world_fre@mail.ru>
2025-08-06 00:46:50 +0300

    update styles

--- backend/database.py
+++ backend/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://veer_user:2gecf232gecf2@localhost:5432/veer_card"
-# DATABASE_URL = "postgresql://postgres:2gecf232gecf2@localhost:5432/veer_card"
+# DATABASE_URL = "postgresql://veer_user:2gecf232gecf2@localhost:5432/veer_card"
+DATABASE_URL = "postgresql://postgres:2gecf232gecf2@localhost:5432/veer_card"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
